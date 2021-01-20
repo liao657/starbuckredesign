f762c61a6a7cf12a8fa3cc53d48536172240a1bf
liao657 <76683675+liao657@users.noreply.github.com>
2021-01-20 19:10:54 +0800

    Create 星巴克.js

---
+++ 星巴克.js
@@ -0,0 +1,14 @@
+$("#banner").slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    autoplay:true,
+    autoplayspeed:1.5,
+});
+$("#service").slick({
+    speed:500,
+    autoplay:true,
+    autoplayspeed:1.5,
+    slidesToShow:3,
+})
